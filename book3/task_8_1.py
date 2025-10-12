4e6116573decda3f5249511e43f2fd1db53391fa
Edgar Amiraghyan <amiraghyan.edgar@gmail.com>
2025-10-12 20:57:47 +0400

    Add Task 8.1

---
+++ book3/task_8_1.py
@@ -0,0 +1,13 @@
+class MatchOperations:
+    
+    @staticmethod
+    def add (a,b):
+        return a+b
+    
+    @staticmethod
+    def multiply(a,b):
+        return a*b
+
+print(MatchOperations.add(4,3))
+print(MatchOperations.multiply(2,3))
+
